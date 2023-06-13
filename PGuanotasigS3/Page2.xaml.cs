feaee24e6aa0cbd94226740f522c44fa0f27069c
master <master@SMLAB306PC22_23>
2023-06-12 21:04:08 -0500

    Agregar archivos de proyecto.

---
+++ PGuanotasigS3/Page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PGuanotasigS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string dato1,double dato2)
+        {
+            InitializeComponent();
+            lblDatoUno.Text = "DATO UNO" + dato1;
+            lblDatoDos.Text = "DATO DOS" + dato2.ToString();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
